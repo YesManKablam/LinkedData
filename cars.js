fe37f3c3a00ab1195065aa38e2d7ca9f05e7d4ce
YesMAnKablam <conorkenny183@gmail.com>
2015-11-21 11:21:25 +0000

    Big update

--- cars.js
+++ cars.js
@@ -4,21 +4,28 @@
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
 var bodyparser = require('body-parser');
+var prompt = require('prompt');
 var fs = require('fs');
 var app = express();
+var path = require("path");
+//var jsonParser = bodyParser.json();
 app.use(bodyparser.json()); // support json encoded bodies
 app.use(bodyparser.urlencoded({ extended: true }));
 
 //  Reads in the .json files
+//================================================================
 var households = JSON.parse(fs.readFileSync('households.json','utf8'))
 var roadaccidents = JSON.parse(fs.readFileSync('RoadAccidents.json','utf8'))
+//================================================================
 
 //  Creating the Database
+//================================================================
 var db = new sqlite3.Database(':memory:');
 db.serialize(function()
 {
   //  Creating the first table for the households that have access to cars in 2011
-  db.run('CREATE TABLE households (Hid INTEGER PRIMARY KEY AUTOINCREMENT, ID REAL, County TEXT, Households REAL)');
+  //================================================================
+  db.run('CREATE TABLE households (ID TEXT, County TEXT, Households REAL)');
   var stmt = db.prepare('INSERT INTO households (ID,County,Households) VALUES (?,?,?)');
   for (var i = 0; i < households.length; i++)
   {
@@ -28,8 +35,11 @@ db.serialize(function()
               );
   }
   stmt.finalize();
+  //================================================================
+
   //  Creating the second Database for the amount of road accidents in 2011
-  db.run('CREATE TABLE roadaccidents (RAid INTEGER PRIMARY KEY AUTOINCREMENT, ID REAL, County TEXT, Deaths REAL, Injuries REAL)');
+  //================================================================
+  db.run('CREATE TABLE roadaccidents (ID REAL, County TEXT, Deaths REAL, Injuries REAL)');
   var stmt = db.prepare('INSERT INTO roadaccidents (ID,County,Deaths,Injuries) VALUES (?,?,?,?)');
   for (var i = 0; i < roadaccidents.length; i++)
   {
@@ -40,6 +50,7 @@ db.serialize(function()
               );
   }
   stmt.finalize();
+  //================================================================
 
   //  Test of both tables to the console
   db.each("SELECT households.*, roadaccidents.* FROM households INNER JOIN roadaccidents ON households.County = roadaccidents.County", function(err, row){
@@ -47,23 +58,57 @@ db.serialize(function()
   });
 });
 
-//  Pushes the result to the browser
-//
-var posts = [];
-db.serialize(function() {
-    db.each("SELECT * FROM households", function(err, row) {
-        posts.push({County: row.County, ID: row.ID, Households: row.Households})
-    }, function() {
+
+// Testing User input via Prompt package
+//================================================================
+prompt.start();   //  Starts the prompt
+
+prompt.get(['id', 'cty', 'house'], function(err, result){
+  if (err) { return onErr(err); }
+  console.log('You typed in: ' + result.id + ' ' + result.cty + ' ' + result.house);
+  db.all("INSERT INTO households (ID, County, Households) VALUES ('"+result.id+"', '"+result.cty+"', '"+result.house+"')", function(err, row) {
+    //res.sendStatus("DELETED");
+  })
+});
+
+function onErr(err){
+  console.log(err);
+  return 1;
+}
+
+//================================================================
+
+//  Pushes to browser using JSON
+app.get('/accessToCars', function(req, res){
+  db.all("SELECT * FROM households", function(err, row) {
+    rowString = JSON.stringify(row, null, '\t');
+    res.sendStatus(rowString);
+  })
+})
+
+app.get('/allRoadAccidents', function(req, res){
+  db.all("SELECT * FROM roadaccidents", function(err, row) {
+    rowString = JSON.stringify(row, null, '\t');
+    res.sendStatus(rowString);
+  })
+})
+
+app.get('/cty/:cty', function(req, res){
+    db.all('SELECT households.*, roadaccidents.* FROM households, roadaccidents ON households.County = roadaccidents.County AND (households.County LIKE "Galway")', function(err, row){
+    //WHERE County LIKE "' + req.params.cty + '"',
+      rowString = JSON.stringify(row, null, '\t');
+      res.sendStatus(rowString);
     })
 })
 
-app.get('/', function(req, res){
-  res.send(posts);
-});
+app.get('/delete/:cty', function(req, res){
+    console.log("ABOUT TO DELETE");
+    db.serialize(function(){
+      db.all('DELETE FROM households WHERE County LIKE "' + req.params.cty + '"',  function(err, row){
+        console.log("DELETED");
+        res.sendStatus("DELETED " + req.params.cty);
+      })
+    })
+})
 
-app.get('/', function(req, res){
-  res.send(posts);
-});
-
-//db.close();
 var server = app.listen(8000);
